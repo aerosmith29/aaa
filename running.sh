bb4b72b00cd52d64b1a71daa189be73c3d85bf5f
aerosmith29 <82898813+aerosmith29@users.noreply.github.com>
2024-03-11 09:17:07 +0700

    Update running.sh

--- running.sh
+++ running.sh
@@ -316,7 +316,7 @@ kernelku=$(uname -r)
 Domen="$(cat /etc/xray/domain)"
 echo -e ""
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\E[44;1;39m            ⇱ KANG HORY VPN TUNNELING ⇲           \E[0m"
+echo -e "\E[44;1;39m            ⇱ AEROSMITH VPN TUNNELING ⇲           \E[0m"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "❇️ Hostname    : $HOSTNAME"
 echo -e "❇️ OS Name     : $Tipe"
