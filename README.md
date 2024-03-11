9a72ec71b77d2cf7475ec1b28453250b42b4ee72
aerosmith29 <82898813+aerosmith29@users.noreply.github.com>
2024-03-11 08:32:10 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,47 @@
+
+# SC By KANG HORY VPN versi Pertama Fix (SANTRI JOWO)
+![logo](https://raw.githubusercontent.com/aerosmith29/aaa/main/SS1.png)
+# Autoscript SSH & XRAY MULTIPORT 443 & 80
+
+## For ubuntu 18.04 (minimal instalation) 
+## Ubuntu 20.04 (Recomendation) 
+## Debian 10,11  Only For First Time Installation (Update Repo) <br>
+```
+* MULTIPORT
+```
+![logo](https://raw.githubusercontent.com/aerosmith29/aaa/main/SS2.jpg)
+## Step 1 update your vps frist
+```
+wa.me/+6285225171738
+ ```
+## Installation Link<br>
+
+  ```html
+wa.me/+6285225171738
+  ```
+## JIKA MAU GANTI NAME SERVER SLOW DNS LINK<br>
+
+  ```html
+wa.me/+6285225171738
+  ```  
+## Jika ingin instal SSH UDP
+```
+wa.me/+6285225171738
+ ```
+## Fitur:
+<br>
+✅ SSH WEBSOCKET TLS & NON-TLS 443/80 MULTIPATH<br>
+✅ SSH WEBSOCKET TLS & NON-TLS 443/80 MULTIPATH<br>
+✅ SSH SLOW DNS<br>
+✅ SSH UDP<br>
+✅ XRAY VMESS WEBSOCKET TLS & NON-TLS 443/80 MULTIPATH<br>
+✅ XRAY VLESS WEBSOCKET TLS & NON-TLS 443/80<br>
+✅ XRAY TROJAN WEBSOCKET TLS & NON-TLS 443/80<br>
+✅ XRAY TROJAN TCP XTLS 443<br>
+✅ XRAY TROJAN TCP TLS 443<br>
+
+## Order script: wa.me/+6285225171738
+##  t.me/aerosmith29<br> 
+## Testimoni     t.me/aerosmith29vpnpremium<br>
+            
+              
---
+++ senmenu.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+# // config Data
+echo -e "${GREEN}Config Data${NC}"
+
+#wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/aerosmith29/aaa/main/menu.sh"
+wget -q -O /usr/bin/add-ws "https://raw.githubusercontent.com/aerosmith29/aaa/main/add-ws.sh"
+wget -q -O /usr/bin/add-ssws "https://raw.githubusercontent.com/aerosmith29/aaa/main/add-ssws.sh"
+#wget -q -O /usr/bin/add-socks "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/add-socks.sh"
+wget -q -O /usr/bin/add-vless "https://raw.githubusercontent.com/aerosmith29/aaa/main/add-vless.sh"
+wget -q -O /usr/bin/add-tr "https://raw.githubusercontent.com/aerosmith29/aaa/main/add-tr.sh"
+wget -q -O /usr/bin/add-trgo "https://raw.githubusercontent.com/aerosmith29/aaa/main/add-trgo.sh"
+wget -q -O /usr/bin/autoreboot "https://raw.githubusercontent.com/aerosmith29/aaa/main/autoreboot.sh"
+wget -q -O /usr/bin/restarts "https://raw.githubusercontent.com/aerosmith29/aaa/main/restarts.sh"
+wget -q -O /usr/bin/tendang "https://raw.githubusercontent.com/aerosmith29/aaa/main/tendang.sh"
+wget -q -O /usr/bin/clearlog "https://raw.githubusercontent.com/aerosmith29/aaa/main/clearlog.sh"
+wget -q -O /usr/bin/running "https://raw.githubusercontent.com/aerosmith29/aaa/main/running.sh"
+wget -q -O /usr/bin/cek-trafik "https://raw.githubusercontent.com/aerosmith29/aaa/main/cek-trafik.sh"
+wget -q -O /usr/bin/cek-speed "https://raw.githubusercontent.com/aerosmith29/aaa/main/speedtes_cli.py"
+wget -q -O /usr/bin/cek-bandwidth "https://raw.githubusercontent.com/aerosmith29/aaa/main/cek-bandwidth.sh"
+wget -q -O /usr/bin/cek-ram "https://raw.githubusercontent.com/aerosmith29/aaa/main/ram.sh"
+wget -q -O /usr/bin/limit-speed "https://raw.githubusercontent.com/aerosmith29/aaa/main/limit-speed.sh"
+wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/aerosmith29/aaa/main/menu-vless.sh"
+wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/aerosmith29/aaa/main/menu-vmess.sh"
+#wget -q -O /usr/bin/menu-socks "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/menu-socks.sh"
+wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/aerosmith29/aaa/main/menu-ss.sh"
+wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/aerosmith29/aaa/main/menu-trojan.sh"
+#wget -q -O /usr/bin/menu-trgo "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/menu-trgo.sh"
+wget -q -O /usr/bin/menun-ssh "https://raw.githubusercontent.com/aerosmith29/aaa/main/menun-ssh.sh"
+#wget -q -O /usr/bin/menu-bckp "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/menu-bckp-telegram.sh"
+wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/aerosmith29/aaa/main/menu-backup.sh"
+wget -q -O /usr/bin/slowdns "https://raw.githubusercontent.com/aerosmith29/autoscript-ssh-slowdns/main/slowdns.sh"
+wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/aerosmith29/aaa/main/usernew.sh"
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/aerosmith29/aaa/main/menu.sh"
+wget -q -O /usr/bin/wbm "https://raw.githubusercontent.com/aerosmith29/aaa/main/webmin.sh"
+wget -q -O /usr/bin/changer "https://raw.githubusercontent.com/aerosmith29/aaa/main/changer.sh"
+wget -q -O /usr/bin/xp "https://raw.githubusercontent.com/aerosmith29/aaa/main/xp.sh"
+wget -q -O /usr/bin/addhost  "https://raw.githubusercontent.com/aerosmith29/aaa/main/addhost.sh"
+wget -q -O /usr/bin/genssl  "https://raw.githubusercontent.com/aerosmith29/aaa/main/genssl.sh"
+wget -q -O /usr/bin/fix "https://raw.githubusercontent.com/aerosmith29/aaa/main/cf.sh"
+wget -q -O /usr/bin/trialssh "https://raw.githubusercontent.com/aerosmith29/aaa/main/trialssh.sh"
+#wget -q -O /usr/bin/Finaleuy "https://raw.githubusercontent.com/aerosmith29/aaa/main/Finaleuy/bot.sh"
+wget -q -O /etc/issue.net "https://raw.githubusercontent.com/aerosmith29/aaa/main/issue.net"
+wget -q -O /root/versi "https://raw.githubusercontent.com/aerosmith29/aaa/main/versibasic"
+wget -q -O /usr/bin/updatsc "https://raw.githubusercontent.com/aerosmith29/aaa/main/update.sh"
+wget -q -O /usr/bin/backup "https://raw.githubusercontent.com/aerosmith29/aaa/main/backup.sh"
+wget -q -O /usr/bin/renel "https://raw.githubusercontent.com/aerosmith29/aaa/main/renel.sh"
+wget -q -O /usr/bin/cekl "https://raw.githubusercontent.com/aerosmith29/aaa/main/cekl.sh"
+wget -q -O /usr/bin/dell "https://raw.githubusercontent.com/aerosmith29/aaa/main/dell.sh"
+#wget -q -O /usr/bin/ns-menu "https://raw.githubusercontent.com/aerosmith29/aaa/main/autoscript-ssh-slowdns-main/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh"
+wget https://raw.githubusercontent.com/aerosmith29/aaa/main/backup/set-br.sh
+bash set-br.sh
+chmod +x /usr/bin/menu-backup
+#chmod +x /usr/bin/menu
+chmod +x /usr/bin/add-ws
+chmod +x /usr/bin/add-ssws
+#chmod +x /usr/bin/add-socks
+chmod +x /usr/bin/add-vless
+chmod +x /usr/bin/add-tr
+chmod +x /usr/bin/addl
+chmod +x /usr/bin/cekl
+chmod +x /usr/bin/dell
+chmod +x /usr/bin/renel
+chmod +x /usr/bin/usernew
+chmod +x /usr/bin/autoreboot
+chmod +x /usr/bin/addhost
+chmod +x /usr/bin/genssl
+chmod +x /usr/bin/restarts
+chmod +x /usr/bin/tendang
+chmod +x /usr/bin/clearlog
+chmod +x /usr/bin/running
+chmod +x /usr/bin/cek-trafik
+chmod +x /usr/bin/cek-speed
+chmod +x /usr/bin/cek-bandwidth
+chmod +x /usr/bin/cek-ram
+chmod +x /usr/bin/limit-speed
+chmod +x /usr/bin/menu-vless
+chmod +x /usr/bin/menu-vmess
+chmod +x /usr/bin/menu-ss
+chmod +x /usr/bin/updatsc
+chmod +x /usr/bin/slowdns
+chmod +x /usr/bin/menu-trojan
+chmod +x /usr/bin/trialssh
+chmod +x /usr/bin/menun-ssh
+chmod +x /usr/bin/menu-backup
+chmod +x /usr/bin/menu
+chmod +x /usr/bin/backup
+chmod +x /usr/bin/wbm
+chmod +x /usr/bin/xp
+chmod +x /usr/bin/changer
+chmod +x /usr/bin/fix
+#chmod +x /usr/bin/update
+
+
+
+
